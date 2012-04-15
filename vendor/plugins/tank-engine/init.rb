177a76c7f4be81e9af1222e7346c567ca27255c8
Eduard Litau <eduard.litau@gmail.com>
2012-04-15 19:31:04 +0200

    removing outdated plugins

--- vendor/plugins/tank-engine/init.rb
+++
@@ -1,4 +0,0 @@
-require File.dirname(__FILE__) + '/lib/tank_engine_helper.rb'
-require File.dirname(__FILE__) + '/lib/tank_engine_controller'
-require File.dirname(__FILE__) + '/lib/tank_engine_form_builder_extensions'
-Mime::Type.register_alias "text/html", :iphone
--- vendor/plugins/tank-engine/test/tank_engine_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test/unit'
-
-class TankEngineTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
-  end
-end
